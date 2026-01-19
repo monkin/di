0174069f56818475a7c0f1518396b60e972784bf
Andrei Monkin <monkin.andrey@gmail.com>
2026-01-19 05:04:00 +0200

    Update `tsconfig.json` and `vite.config.ts`: add comment removal during builds, enable code minification

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,42 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import dts from 'vite-plugin-dts';
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+import { transform } from "esbuild";
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/di-sacala.ts'),
-      name: 'DiSacala',
-      fileName: 'index',
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/di-sacala.ts"),
+            name: "DiSacala",
+            fileName: "index",
+        },
+        minify: true,
     },
-  },
-  plugins: [dts({ exclude: ['**/*.test.ts'] })],
+    plugins: [
+        dts({ exclude: ["**/*.test.ts"] }),
+        {
+            name: "remove-comments",
+            async renderChunk(code) {
+                const result = await transform(code, {
+                    minify: true,
+                    legalComments: "none",
+                });
+                return {
+                    code: result.code,
+                    map: result.map,
+                };
+            },
+        },
+        {
+            name: "force-remove-comments",
+            generateBundle(_options, bundle) {
+                for (const fileName in bundle) {
+                    const file = bundle[fileName];
+                    if (file.type === "chunk") {
+                        file.code = file.code.replace(/\/\*[\s\S]*?\*\//g, "");
+                    }
+                }
+            },
+        },
+    ],
 });
